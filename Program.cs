0a19f0086988a5d0fb3cd369e9e6500f6d3798ca
Emre KANARYA <61049640+kanarya4909@users.noreply.github.com>
2020-05-16 10:04:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi1 = 10;
+
+            //int sayi2 = 20;
+
+            //int toplam;
+
+            //toplam = sayi1 + sayi2;
+
+            //CTRL TUŞU+"K" VE "C" TUŞUNA BASILI TUTTUĞUNDA SEÇİLİ ALANLARIN HEPSİ YORUM SATIRI HALİNE GELİYOR.
+
+            //"K" + "U" AYNI ANDA BASARSAN YORUM SATIRLARI KALKMIŞ OLUP NOORMAL KODA DÖNÜŞÜYOR.
+
+            //String İsim = "Sadık";
+
+            //string Soyisim = " Turan";  // Burda dikkat edersen bir tane boşluk bıraktım "turanda" çünkü çıktıda isim ile soyisim bitişik gözükmesin.
+
+            //bool secim; // "bool" veri türü True veya False değerlerini alırlar bu veri türü bize bir işlemin sonucunu doğru veya yanlış, var veya yok olduğunu gösterir.
+
+            //secim = true;
+
+            //secim = false; // Burda dikkat edilmesi gereken kısım en son kısmdaki yani bu kısımdaki secimde "false" olduğu için çıktıda "false" oldu.
+
+                Console.WriteLine(secim);
+
+            Console.ReadLine();
+        }
+    }
+}
